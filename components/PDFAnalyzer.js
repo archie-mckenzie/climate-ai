7933f8929520f38a55196ca939724a0d10194f3d
Archie McKenzie <archiem@princeton.edu>
2023-11-11 12:33:03 -0500

    latest updates

--- components/PDFAnalyzer.js
+++ components/PDFAnalyzer.js
@@ -8,43 +8,32 @@ export default function PDFAnalyzer() {
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
+        alert('Hello')
     };
 
     const handleButtonClick = () => {
         fileInputRef.current.click();
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (selectedFile) {
-            console.log('File ready for upload: ', selectedFile);
-            // You can handle the file upload process here
-        }
-    };
-
     return (
         <div className='pdf-analysis-container'>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <button 
-                        type="button" 
-                        className='choose-file-button'
-                        onClick={handleButtonClick}
-                    >
-                        ⬆️&nbsp;&nbsp;Upload PDF
-                    </button>
-                    <input
-                        type="file"
-                        accept=".pdf"
-                        onChange={handleFileChange}
-                        ref={fileInputRef}
-                        style={{ display: 'none' }}
-                    />
-                </div>
-                <br/>
-                {selectedFile && <button type="submit">Upload PDF</button>}
-            </form>
-            {selectedFile && <p>File ready for upload: {selectedFile.name}</p>}
+            <div>
+                <button 
+                    type="button" 
+                    className={`choose-file-button ${selectedFile ? 'highlighted-background' : ''}`}
+                    onClick={handleButtonClick}
+                >
+                    ⬆️&nbsp;&nbsp;Upload PDF
+                </button>
+                <input
+                    type="file"
+                    accept=".pdf"
+                    onChange={handleFileChange}
+                    ref={fileInputRef}
+                    style={{ display: 'none' }}
+                />
+            </div>
+            {selectedFile && <p>{selectedFile.name}</p>}
         </div>
     );
 }
