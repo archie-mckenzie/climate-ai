a6a249d2129fbd5151ec55e86ad82c199a106f89
faviles28 <faviles@princeton.edu>
2023-11-12 10:00:17 -0500

    stuff

--- components/GreenalysisDisplay.js
+++ components/GreenalysisDisplay.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 export default function GreenalysisDisplay({job}) {
+    const totalRevenueSum = job.annualReport.reduce((sum, report) => sum + report.totalRevenue, 0);
     return (
         <div className="links">
             { job.company_name &&
@@ -7,7 +8,19 @@ export default function GreenalysisDisplay({job}) {
             }
             {
                 job.net_carbon_emissions &&
-                <p><b>Net Carbon Emissions: {job.net_carbon_emissions}</b></p>
+                <p><b>Net Carbon Emissions: {job.net_carbon_emissions} metric tons</b></p>
+            }
+            {
+                job.annualReport &&
+                <p><b>Emissions Per Dollar of Revenue in 2022: {job.net_carbon_emissions / job.annualReport[1].totalRevenue}</b></p>
+            }
+            {
+                job.annualReport && job.net_carbon_emissions &&
+                <p><b>Emissions Past 5 Years: {totalRevenueSum*(job.net_carbon_emissions / job.annualReport[1].totalRevenue)}</b></p>
+            }
+            {
+                job.annualReport &&
+                <p><b>Total Cost of Emissions Past 5 Years: {88*10*10*10*10*10*10*totalRevenueSum}</b></p>
             }
             {
                 job.summary &&
