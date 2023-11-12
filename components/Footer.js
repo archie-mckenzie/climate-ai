ebad154836f441b373e0dca5714b6cc5d38755e7
Archie McKenzie <archiem@princeton.edu>
2023-11-11 22:13:11 -0500

    contacts

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,12 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-      <footer className="footer">
-        <p>© {new Date().getFullYear()} <Link className='subtle-link highlighting' href='mailto:archie@greenalysis.org'>AM</Link>, <Link className='subtle-link highlighting' href='mailto:fernando@greenalysis.org'>FA</Link>, <Link className='subtle-link highlighting' href='mailto:ally@greenalysis.org'>AD</Link>, <Link className='subtle-link highlighting' href='mailto:benedikt@greenalysis.org'>BS</Link></p>
+      <footer className="links">
+        <Link href='/greenalyses' className='subtle-link highlighting'>✅ See completed greenalyses</Link>
+        <br/>
+        <Link href='/team' className='subtle-link highlighting'>👷‍♂️ Meet the team</Link>
+        <br/>
+        ✉️ Contact us: <Link className='subtle-link highlighting' href='mailto:archie@greenalysis.org'>AM</Link>, <Link className='subtle-link highlighting' href='mailto:fernando@greenalysis.org'>FA</Link>, <Link className='subtle-link highlighting' href='mailto:ally@greenalysis.org'>AD</Link>, <Link className='subtle-link highlighting' href='mailto:benedikt@greenalysis.org'>BS</Link>
       </footer>
     );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Header from "../../components/Header"
 import Footer from "../../components/Footer"
 import PDFAnalyzer from "../../components/PDFAnalyzer"
+import Link from "next/link"
 
 export default function Home() {
   return (
