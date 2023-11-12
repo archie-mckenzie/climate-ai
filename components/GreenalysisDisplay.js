55861c60b53b9182c61fee8365665dc78a0e2519
faviles28 <faviles@princeton.edu>
2023-11-12 10:10:27 -0500

    finito

--- components/GreenalysisDisplay.js
+++ components/GreenalysisDisplay.js
@@ -11,15 +11,15 @@ export default function GreenalysisDisplay({job}) {
             }
             {
                 job.annualReport &&
-                <p><b>Emissions Per Dollar of Revenue in 2022: {job.net_carbon_emissions / job.annualReport[1].totalRevenue}</b></p>
+                <p><b>Emissions Per Dollar of Revenue in 2022: {job.net_carbon_emissions / job.annualReport[1].totalRevenue} metric tons</b></p>
             }
             {
                 job.annualReport && job.net_carbon_emissions &&
-                <p><b>Emissions Past 5 Years: {(job.annualReport.reduce((sum, report) => sum + report.totalRevenue, 0))*(job.net_carbon_emissions / job.annualReport[1].totalRevenue)}</b></p>
+                <p><b>Emissions Past 5 Years: {(job.annualReport.reduce((sum, report) => sum + report.totalRevenue, 0))*(job.net_carbon_emissions / job.annualReport[1].totalRevenue)} metric tons</b></p>
             }
             {
                 job.annualReport &&
-                <p><b>Total Cost of Emissions Past 5 Years: {88*10*10*10*10*10*10*(job.annualReport.reduce((sum, report) => sum + report.totalRevenue, 0))}</b></p>
+                <p><b>Total Cost of Emissions Past 5 Years: ${88*10*10*10*10*10*10*(job.annualReport.reduce((sum, report) => sum + report.totalRevenue, 0))}</b></p>
             }
             {
                 job.summary &&
